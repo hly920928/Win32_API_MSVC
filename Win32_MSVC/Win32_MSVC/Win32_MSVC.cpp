895f1e305dbee7d902f87eca16da8653663db24d
hly920928 <hly920928@gmail.com>
2018-01-17 17:41:38 +0800

    to_upper Exception Handler using C++ try And Catch

--- Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
+++ Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
@@ -4,33 +4,77 @@
 #include <cstdio>
 #include "Everything.h" 
 #include "myHeader_V2.h" 
-#include "test.h" 
 int main(int argc, LPCSTR argv[])
 {
-	printf("%s\n", argv[0]);
-	printf("%d\n", argc);
-	for (int i = 1; i < argc; i++) 
-		printf("%s\n", argv[i]);
-	if (argv[1] == NULL)
-	{
-		printf("No file Name\n");
+	HANDLE hIn = INVALID_HANDLE_VALUE, hOut = INVALID_HANDLE_VALUE;
+	DWORD nXfer, iFile, j;
+	CHAR outFileName[256] = "", *pBuffer = NULL;
+	OVERLAPPED ov = { 0, 0, 0, 0, NULL };
+	LARGE_INTEGER fSize;
+	if (argc <= 1) {
+		printf("Usage: to_upper files Please Input file name\n");
 		return 0;
 	}
-	//char name[] = "123.txt\0";
-	//printf(" file Name %s\n", name);
-	HANDLE hIn = CreateFileA(argv[1], GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hIn == INVALID_HANDLE_VALUE) {
-		printf("Open file fail\n");
-		return 0;
+	for (iFile = 1; iFile < (unsigned int)argc; iFile++)
+		try {
+		if (strlen(argv[iFile]) > 250) {
+			printf("The file name is too long./n");
+			throw 1;
+		}
+		sprintf(outFileName, "UC_%s", argv[iFile]);
+		try {
+				hIn = CreateFileA(argv[iFile], GENERIC_READ,
+					0, NULL, OPEN_EXISTING, 0, NULL);
+				if (hIn == INVALID_HANDLE_VALUE) {
+					printf("%d Invalid\n", argv[iFile]);
+					throw 2;
+				}
+				if (!GetFileSizeEx(hIn, &fSize) || fSize.HighPart > 0) {
+						printf("%s is too large for this program\n", argv[iFile]);
+						throw 3;
+				}
+				hOut = CreateFileA(outFileName, GENERIC_READ | GENERIC_WRITE,
+					0, NULL, CREATE_NEW, 0, NULL);
+				if (hOut == INVALID_HANDLE_VALUE) {
+					printf("%s open fail\n", argv[iFile]);
+					throw 4;
+				}
+				pBuffer =(CHAR*) malloc(fSize.LowPart);
+				if (pBuffer == NULL) {
+					printf("%s malloc fail\n", argv[iFile]);
+					throw 5;
+				}
+				if (!ReadFile(hIn, pBuffer, fSize.LowPart, &nXfer, NULL) || (nXfer != fSize.LowPart))
+				{
+					printf("%s Read fail\n", argv[iFile]);
+					throw 6;
+				}
+				for (j = 0; j < fSize.LowPart; j++)
+					if (isalpha(pBuffer[j])) pBuffer[j] = toupper(pBuffer[j]);
+				if (!WriteFile(hOut, pBuffer, fSize.LowPart, &nXfer, NULL) || (nXfer != fSize.LowPart))
+				{
+					printf("%s Write fail\n", argv[iFile]);
+					throw 6;
+				}
+
+			}
+			catch(...){}
+			if (pBuffer != NULL) free(pBuffer); pBuffer = NULL;
+			if (hIn != INVALID_HANDLE_VALUE) {
+				CloseHandle(hIn);
+				hIn = INVALID_HANDLE_VALUE;
+			}
+			if (hOut != INVALID_HANDLE_VALUE) {
+				CloseHandle(hOut);
+				hOut = INVALID_HANDLE_VALUE;
+			}
+			strcpy(outFileName, "");
+		}
+	catch (...) {
+		printf("Error occured processing file %s\n", argv[iFile]);
+		DeleteFileA(outFileName);
 	}
-    DWORD nln = 0;
-	char aBuffer[99];
-	ReadFile(hIn, aBuffer, BUF_SIZE, &nln, NULL);
-	printf("nIn = %d\n", nln);
-	aBuffer[nln] = '\0';
-	printf_s("%s\n", aBuffer); 
-	
-	    
+	printf("All files converted\n");
     return 0;
-}
+};
 
