94d950dca5d49f983cee4fcb9165ea1724c67f13
hly920928 <hly920928@gmail.com>
2018-01-27 22:47:10 +0800

    Exercise On Process // PrintOut.exe

--- Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
+++ Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
@@ -8,21 +8,15 @@
 typedef void(*ptr1)(char*) ;
 int main(int argc, LPCSTR argv[])
 {	
-	ptr1 pPrintOut1;
-	ptr1 pPrintOut2;
-	HMODULE hDll = LoadLibraryA("DllTest.dll");
-	if (hDll == NULL) {
-		printf("LoadDll fail\n"); return 0;
-	}
-	pPrintOut1 = (ptr1)GetProcAddress(hDll,"printOut_V2");
-	pPrintOut2 = (ptr1)GetProcAddress(hDll, "printOut_V1");
-	if (pPrintOut1 == NULL|| pPrintOut2 == NULL) {
-		printf("GetProcAddress fail\n"); return 0;
-	}
-	pPrintOut1((char*)argv[0]);
-	pPrintOut2((char*)argv[0]);
-	printf("End\n");
-	FreeLibrary(hDll);
+	DWORD pid =GetCurrentProcessId();
+	printf("PID is %d ", pid);
+	if (argc == 1) {
+		printf("No second Command line argv\n");
+		ExitProcess(0);
+		return 0;
+   }
+	printf("Caller is %s\n",argv[1]);
+	ExitProcess(1);
 	return 0;
 }
 
