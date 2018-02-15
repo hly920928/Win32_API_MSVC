658f521dbec0e532f4cd00a11502a21cbf9a9fb3
hly920928 <hly920928@gmail.com>
2018-02-16 02:30:45 +0800

    Begin SortMT

--- Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
+++ Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
@@ -10,72 +10,104 @@
 #include "jobsMngmt.h";
 #include "helperFunction.h"
 using namespace std;
-struct thread_arg {
-	int id;
-	vector<vector<string>>* output;
-	string file_n;
-	string* pattern;
-};
-static DWORD WINAPI patternSearch_MT(thread_arg* pArgs);
-int main(int argc, LPCSTR argv[])
-{
-	//output.resize(argc - 2);
-	if (argc < 3) {
-		printf("Input_Error\n");
-		return 0;
+struct Record {
+	char t[10];
+	char operator[](int i)const {
+		return t[i];
 	}
-	vector<vector<string>>output;
-	output.resize(argc-2);
-	string pattern = argv[1];
-	HANDLE tHandle[1024];
-	int threadCount = argc - 2;
-	for (int i = 0; i < threadCount;i++) {
-		thread_arg* p_thread_arg = new thread_arg();
-		p_thread_arg->id = i;
-		p_thread_arg->output = &output;
-		p_thread_arg->pattern = &pattern;
-		p_thread_arg->file_n = argv[2+i];
-		tHandle[i] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)patternSearch_MT, p_thread_arg, 0, NULL);
+	char& operator[](int i) {
+		return t[i];
 	}
-	while (threadCount > 0) {
-		int tID= WaitForMultipleObjects(threadCount, tHandle, FALSE, INFINITE);
-		printf("Search %s Finish\n", argv[2 + tID]);
-		printf("Result = \n", argv[2 + tID]);
-		for (auto& s : output[tID]) {
-			printf("%s\n", s.data());
+	void printOut()const {
+		for (int i = 0; i < 8; i++) {
+			printf("%c", t[i]);
 		}
 		printf("\n");
-		threadCount--;
 	}
-	for (int i = 0; i < threadCount; i++) {
-		CloseHandle(tHandle[i]);
+	bool operator <(const Record&  b) {
+		for (int i = 0; i < 8; i++) {
+			if (t[i] < b.t[i])return true;
+			if (t[i] > b.t[i])return false;
+		}
+		return false;
 	}
-	int a = 6;
+};
+struct sort_thread_arg {
+	unsigned char id;
+	unsigned char threadNum;
+	Record* head;
+	Record* end;
+};
+void RandomRecord(Record* header, int n) {
+	for (int i = 0; i < n; i++) {
+		auto& temp= *header;
+		for (int t = 0; t < 8; t++) {
+			int tt = rand() % 10;
+			temp[t] = tt+'0';
+		}
+		temp[8] = '\0';
+		temp[9] = '\r\n';
+		header++;
+	}
+}
+static DWORD WINAPI sort_MT(sort_thread_arg* pArgs);
+int main(int argc, LPCSTR argv[])
+{
+	if (argc < 3) {
+		printf("Command Line incorrent\n");
+		return 0;
+	}
+	char fileName[999];
+	strcpy(fileName, argv[1]);
+	int threadNum = atoi(argv[2]);
+	
+	//check threadNum
+	{
+		int threadNumber[] = {
+			1,2,4,8,16,32,64,128,256 };
+		bool f1 = false;
+		for (int i : threadNumber) {
+			if (threadNum == i) {
+				f1 = true;
+				break;
+			}
+		}
+		if (!f1) {
+			printf("threadNum InValid\n");
+			goto END;
+		}
+	}
+	//open file
+	HANDLE hFile = CreateFileA(fileName, GENERIC_READ | GENERIC_WRITE,0, NULL, OPEN_EXISTING, 0, NULL);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		printf("Failure to open input file.\m");
+		goto END;
+	}
+	long long fileSize = -1;
+	GetFileSizeEx(hFile, (PLARGE_INTEGER)&fileSize);
+	int nOfRecord = (fileSize) / sizeof(Record);
+	HANDLE mHandle = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, 0, NULL);
+	if (NULL == mHandle) {
+        printf("Failure to create mapping handle on input file.\n");
+		goto END;
+	}
+	Record* pRecords = (Record*)MapViewOfFile(mHandle, FILE_MAP_ALL_ACCESS, 0, 0,0);
+	RandomRecord(pRecords, nOfRecord);
+	if (NULL == pRecords) {
+		printf("Failure to map input file.\n");
+		goto END;
+	}
+	Record* h = pRecords;
+//Clear up	
+END:
+	UnmapViewOfFile(pRecords);
+	CloseHandle(mHandle);
+	CloseHandle(hFile);
 	return 0;
 }
 
-static DWORD WINAPI patternSearch_MT(thread_arg* pArgs)
+static DWORD WINAPI sort_MT(sort_thread_arg* pArgs)
 {
-	int tid = GetCurrentThreadId();
-	//printf("tid = %d\n", tid);
-	ifstream fi; fi.open(pArgs->file_n);
-	if (fi.fail()) {
-		printf("Open_File_Fail\n");
-	}
-	int id = pArgs->id;
-	string s;
-	string& pt =*(pArgs->pattern); const char* p= nullptr;
-	auto& v1 = *pArgs->output;
-	auto& v2 = v1[id];
-	while (true) {
-		fi >> s;
-		if (fi.eof())break;
-		p=strstr(s.data(), pt.data());
-		if (p != nullptr) {
-			v2.push_back(s);
-		}
-	}
-		delete pArgs;
-		fi.close();
+    
 	return 0;
 }
