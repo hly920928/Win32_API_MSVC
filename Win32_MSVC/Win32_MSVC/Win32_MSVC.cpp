72fce87e915705ae6d914bbb7fd147227aa04a19
hly920928 <hly920928@gmail.com>
2018-02-27 20:30:37 +0800

    A Producer/Consumer System
    Using Win32 Mutex and Event

--- Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
+++ Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
@@ -13,7 +13,8 @@
 #define DATA_SIZE 256
 using namespace std;
 struct msgBlock {
-	CRITICAL_SECTION mGuard;	
+	HANDLE mGuard;	//Mutex HANDLE
+	HANDLE	mReady; // Event  HANDLE
 	DWORD fReady, fStop;
 	volatile DWORD nCons, mSequence; 
 	DWORD nLost;
@@ -30,7 +31,10 @@ int main(int argc, LPCSTR argv[])
 {
 	DWORD status;
 	HANDLE hProduce, hConsume;
-	InitializeCriticalSection(&gobalBlock.mGuard);// Initialize CS
+	// Init  Sync Object
+	gobalBlock.mGuard = CreateMutexA(NULL, FALSE, NULL);
+	gobalBlock.mReady = CreateEventA(NULL, FALSE, FALSE, NULL);
+
 	hProduce = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)Produce, NULL, 0, NULL);
 	if (hProduce == NULL)
 		printf("Cannot create Producer thread\n");
@@ -44,8 +48,9 @@ int main(int argc, LPCSTR argv[])
 	status = WaitForSingleObject(hProduce, INFINITE);
 	if (status != WAIT_OBJECT_0)
 		printf("Failed waiting for Producer thread\n");
-
-	DeleteCriticalSection(&gobalBlock.mGuard);// 	Delete CS
+	// Delete  Sync Object
+	CloseHandle(gobalBlock.mGuard);
+	CloseHandle(gobalBlock.mReady);;
 
 	printf("Producer and Consumer threads have terminated\n");
 	printf("Messages Produced: %d, Consumed: %d, Lost: %d.\n",
@@ -59,16 +64,17 @@ DWORD WINAPI Produce(void* args)
 	auto& local = gobalBlock;
 	while (!gobalBlock.fStop) {
 		Sleep(1500+rand() / 100);
-		EnterCriticalSection(&gobalBlock.mGuard);//Enter CS
+		WaitForSingleObject(gobalBlock.mGuard,INFINITE);//Wait Mutex
 		__try {
 			if (!gobalBlock.fStop) {
 				gobalBlock.fReady = 0;
 				MessageFill(&gobalBlock);
 				gobalBlock.fReady = 1;
 				InterlockedIncrement(&gobalBlock.mSequence);
+				SetEvent(gobalBlock.mReady);//Set Event
 			}
 		}
-		__finally { LeaveCriticalSection(&gobalBlock.mGuard); }//Leave CS
+		__finally { ReleaseMutex(gobalBlock.mGuard); }//Release Mutex
 	}
 	return 0;
 };
@@ -84,18 +90,20 @@ DWORD WINAPI Consume(void* args)
 			gobalBlock.fStop = 1;
 		}
 		else if (command == 'c') {
-			EnterCriticalSection(&gobalBlock.mGuard);
+			WaitForSingleObject(gobalBlock.mReady, INFINITE);//Wait Event
+			WaitForSingleObject(gobalBlock.mGuard, INFINITE);//Wait Mutex In this ORDER
 			__try {
-				if (gobalBlock.fReady == 0)
+				if (gobalBlock.fReady == 0) {
 					printf("No new messages. Try again later\n");
-				else {
+					_leave;
+				}else {
 					MessageDisplay(&gobalBlock);
 					gobalBlock.nLost = gobalBlock.mSequence - gobalBlock.nCons + 1;
 					gobalBlock.fReady = 0; /* No new messages are ready */
 					InterlockedIncrement(&gobalBlock.nCons);
 				}
 			}
-			__finally { LeaveCriticalSection(&gobalBlock.mGuard); }
+			__finally { ReleaseMutex(gobalBlock.mGuard); }//Release Mutex
 		}
 		else {
 			printf("Illegal command. Try again.\n");
