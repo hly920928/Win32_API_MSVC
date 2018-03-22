46ec9aea3fb0b6c2976695c21b025b54a3137109
hly920928 <hly920928@gmail.com>
2018-03-23 02:03:00 +0800

    Anonymous Pipes
    Just templet, Unknown Bug Existing

--- Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
+++ Win32_MSVC/Win32_MSVC/Win32_MSVC.cpp
@@ -6,21 +6,56 @@
 #include <string>
 #include "messages.h"
 using namespace std;
+void randStr(char* buffer) {
+	for (int i = 0; i < 5; i++) {
+		buffer[i] = 'a'+rand() % 26;
+	}
+	buffer[5] = '\n';
+}
 int main(int argc, LPCSTR argv[])
 {
-	HANDLE hRead = GetStdHandle(STD_INPUT_HANDLE);
-	printf("In ReadP\n");
-	char buffer[6];
-	string s;
-	DWORD n_read = 0;
-	while (true) {
-		ReadFile(hRead, buffer, 6, &n_read, NULL);
-		buffer[5] = '\0';
-		s= buffer;
-		if (buffer[0] == '-')break;
-		cout << "In ReadP : " << s << endl;
+	SECURITY_ATTRIBUTES pipeSA = { sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };
+	PROCESS_INFORMATION procInfo1, procInfo2;
+	STARTUPINFOA startInfoCh1, startInfoCh2;
+	GetStartupInfoA(&startInfoCh1);
+	GetStartupInfoA(&startInfoCh2);
+	HANDLE hReadPipe, hWritePipe;
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &pipeSA, 0)) {
+		printf("Pipe Create Fail\n");
+		return 0;
 	}
-	printf("In ReadP END\n");
+	//Process 1
+	startInfoCh1.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+	startInfoCh1.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+	startInfoCh1.hStdOutput = hWritePipe;
+	startInfoCh1.dwFlags = STARTF_USESTDHANDLES;
+	if (!CreateProcessA(NULL, "ReadP_0", NULL, NULL,
+		TRUE,			/* Inherit handles. */
+		0, NULL, NULL, &startInfoCh1, &procInfo1)) {
+		printf("Process 1 Create Fail\n");
+		return 0;
+	}
+	CloseHandle(procInfo1.hThread);
+	CloseHandle(hWritePipe);
+
+	//Process 2
+	startInfoCh2.hStdInput = hReadPipe;
+	startInfoCh2.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+	startInfoCh2.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	startInfoCh2.dwFlags = STARTF_USESTDHANDLES;
+	if (!CreateProcessA(NULL, "ReadP", NULL, NULL,
+		TRUE,			/* Inherit handles. */
+		0, NULL, NULL, &startInfoCh2, &procInfo2)) {
+		printf("Process 2 Create Fail\n");
+		return 0;
+	}
+	CloseHandle(procInfo2.hThread);
+	CloseHandle(hReadPipe);
+
+	WaitForSingleObject(procInfo1.hProcess, INFINITE);
+	WaitForSingleObject(procInfo2.hProcess, INFINITE);
+	CloseHandle(procInfo1.hProcess);
+	CloseHandle(procInfo2.hProcess);
 	return 0;
-}
+};
 
